3034f4b4f4ef273ee1c2e9eaac02f278bfa9d0a0
Gabriel <167330424+Gabrielzinho1518@users.noreply.github.com>
2025-04-04 12:50:49 -0300

    Atividade em Aula

---
+++ Aula05.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cachorro c = new Cachorro();
+            c.nome = "Toby";
+            Console.WriteLine("Cachorro: " + c.nome);
+            c.EmitirSom();
+            c.movimentar(); 
+
+            Gato g = new Gato();
+            g.nome = "Garfield";
+            Console.WriteLine("Gato: " + g.nome);
+            g.EmitirSom();
+            g.movimentar();
+
+            Peixe p = new Peixe();
+            p.nome = "nemo";
+            Console.WriteLine("Peixe: " +p.nome);
+            p.movimentar();
+            p.EmitirSom();
+        }
+    }
+}
